3358148f4ce9fba1f56f3fe3806dbc2ca230a0be
Javier <hernandezmartinezjavier115@gmail.com>
2020-10-14 20:28:05 -0500

    Cambie Laravel por Wolf Music

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Wolf Music
                 </div>
 
                 <div class="links">
